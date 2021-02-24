8e2fcb423d7479b713728b2aa7a44146c2ab6514
himel999 <mahabubulalamofficial@gmail.com>
2021-02-24 18:02:45 +0600

    problem number 34

--- P#2/problem_34.py
+++ P#2/problem_34.py
@@ -5,12 +5,11 @@ i = 1
 while i <= t:
     a, b, c = map(int, input().split())
 
-    for k in range(1, c+1):
+    for k in range(1, c + 1):
 
         if k % a == 0 and k % b == 0:
-
             print(k)
     if i < t:
-       print()
+        print()
 
     i = i + 1
