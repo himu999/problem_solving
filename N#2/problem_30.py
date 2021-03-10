9c027489189495ef6680174a4edae8ee113c12f2
himel999 <mahabubulalamofficial@gmail.com>
2021-03-11 01:26:27 +0600

    problem number 30

--- N#2/problem_30.py
+++ N#2/problem_30.py
@@ -13,7 +13,7 @@ while i <= t:
     for j in range(2, m+1):
 
         if a % j == 0:
-            val = val + j + (a/j)
+            val = val + j + (a / j)
 
     if a == val:
         print("YES, {} is a perfect number!".format(a))
@@ -21,24 +21,3 @@ while i <= t:
         print("NO, {} is not a perfect number!".format(a))
 
     i = i + 1
-
-
-
-
-
-
-
-
-# T = int(input())
-# while T > 0:
-#     n = int(input())
-#     result = 1
-#     m = int(n**0.5)
-#     for i in range(2,m + 1):
-#         if n % i == 0:
-#                 result =result + i + (n/i)
-#     if result == n:
-#         print("YES, {} is a perfect number!".format(n))
-#     else:
-#         print("NO, {} is not a perfect number!".format(n))
-#     T -= 1
